40d44dbaa74892a1be1e9451dd59b7d50009ecad
Jose Luis Cazal <josecazal.1991@gmail.com>
2019-02-24 19:02:45 -0300

    Refactorizamos el Proyecto Mobile

---
+++ app/src/main/java/application/android/josecazal/rrhh/Activitys/loginActivity.java
@@ -0,0 +1,15 @@
+package application.android.josecazal.rrhh.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import application.android.josecazal.rrhh.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
